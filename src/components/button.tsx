ee13be4af6f2f6980b85669ff9edc89ef203b7a4
Veeno <sebastien.cloiseau@gmail.com>
2023-10-26 22:32:00 +0200

    box middle updated

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,14 +3,14 @@ import React from "react";
 interface ButtonProps {
   extraCss?: string;
   manyChildren?: boolean;
-  children: React.ReactNode;
+  children?: React.ReactNode;
   [key: string]: any;
 }
 
 export const Button = ({ extraCss, children, ...props }: ButtonProps) => {
   return (
     <button
-      className={`w-fit px-2 flex items-center text-sm lg:text-xs font-normal justify-center rounded h-8 sm:h-7 bg-light-bg-terciary hover:bg-light-bg-hover dark:bg-dark-bg-terciary dark:hover:bg-dark-bg-hover text-light-font-100 dark:text-dark-font-100 transition border border-light-border-primary dark:border-dark-border-primary ${extraCss}`}
+      className={`w-fit px-2 flex items-center text-sm lg:text-xs font-normal justify-center rounded h-8 sm:h-7 bg-light-bg-terciary hover:bg-light-bg-hover dark:bg-dark-bg-terciary dark:hover:bg-dark-bg-hover text-light-font-100 dark:text-dark-font-100 transition border border-light-border-primary dark:border-dark-border-primary duration-250 ${extraCss}`}
       {...props}
     >
       {children}
--- src/features/data/top100/components/box-middle/index.tsx
+++ src/features/data/top100/components/box-middle/index.tsx
@@ -1,22 +1,18 @@
-/* eslint-disable no-nested-ternary */
-import { Button, Flex } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { useColors } from "../../../../../lib/chakra/colorMode";
+import { Button } from "../../../../../components/button";
 import { BtcDominance } from "./btc-dominance";
 import { CryptoMarket } from "./crypto-market";
 import { FearGreed } from "./fear-greed";
 
-export const BoxMiddle = ({
-  showPageMobile = 0,
-  metrics,
-}: {
+interface BoxMiddleProps {
   showPageMobile?: number;
   metrics: {
     fear_and_greed_value: number;
     fear_and_greed_value_classification: string;
   };
-}) => {
-  const { borders, boxBg3, text40, text80 } = useColors();
+}
+
+export const BoxMiddle = ({ showPageMobile = 0, metrics }: BoxMiddleProps) => {
   const [showPage, setShowPage] = useState(0);
   const tablet =
     (typeof window !== "undefined" ? window.innerWidth : 0) < 991 &&
@@ -46,7 +42,6 @@ export const BoxMiddle = ({
       }
       key="BtcDominance"
     />,
-    // <Test showPage={showPage} />,
   ];
 
   useEffect(() => {
@@ -60,34 +55,29 @@ export const BoxMiddle = ({
 
   return (
     <div
-      className={`flex h-[200px] lg:h-[175px] rounded-xl bg-light-bg-secondary dark:bg-dark-bg-secondary border border-light-border-primary dark:border-dark-border-primary flex-col py-2.5 px-3.5 relative overflow-hidden min-w-[407px] md:min-w-full w-[31.5%] sm:w-full transition duration-500 translate-x-[-${
+      className={`flex h-[200px] lg:h-[175px] rounded-xl bg-light-bg-secondary dark:bg-dark-bg-secondary border border-light-border-primary dark:border-dark-border-primary py-2.5 px-2.5 relative overflow-hidden min-w-[407px] md:min-w-full w-[31.5%] sm:w-full transition duration-500 translate-x-[-${
         showPageMobile * 100
       }%] z-[${showPageMobile === 1 ? 3 : 1}] mx-2.5 md:mx-0`}
     >
-      <Flex
-        align="center"
-        position="absolute"
-        top="0px"
-        right="0px"
-        h="35px"
-        px="15px"
-        bg={boxBg3}
-        zIndex="1"
-      >
+      <div className="flex items-center absolute top-0 right-0 h-[35px] px-[15px] bg-light-bg-secondary dark:bg-dark-bg-secondary z-[1]">
         {render.map((_, idx) => (
           <Button
-            borderRadius="full"
+            extraCss={`rounded-full ${
+              showPage === idx ? "w-[9px]" : "w-[8px]"
+            } ${showPage === idx ? "h-[9px]" : "h-[8px]"} ${
+              showPage === idx ? "max-w-[9px]" : "max-w-[8px]"
+            } ${
+              showPage === idx ? "max-h-[9px]" : "max-h-[8px]"
+            } px-0 ml-[5px] ${
+              showPage === idx
+                ? "bg-light-font-80 dark:bg-dark-font-80"
+                : "bg-light-font-40 dark:bg-dark-font-40"
+            } `}
             key={_.key}
-            boxSize={showPage === idx ? "9px" : "8px"}
-            w={showPage === idx ? "9px" : "8px"}
-            maxW={showPage === idx ? "9px" : "8px"}
-            px="0px"
-            bg={showPage === idx ? text80 : text40}
-            ml="5px"
             onClick={() => setShowPage(idx)}
           />
         ))}
-      </Flex>
+      </div>
       {render}
     </div>
   );
