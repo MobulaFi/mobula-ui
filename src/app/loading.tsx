2ffa512d68201135613d0bc67252aef2ab209e43
Veeno <sebastien.cloiseau@gmail.com>
2023-11-28 03:24:38 +0100

    remove loading page

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return (
-    <div className="bg-red w-[500px] h-[500px] m-auto">
-      Loading
-      <img className="/mobula/mobula-logo.svg" width={123} height={123} />
-    </div>
-  );
-}
