911a15d0f117b789e92d93679ae52f7c410fcc0d
Veeno <sebastien.cloiseau@gmail.com>
2023-12-11 18:39:22 +0100

    fix: glitch UI for menu mobile while opening

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -63,7 +63,7 @@ export const Header = ({ addressCookie }) => {
           </TextLandingSmall>
         </Flex> */}
       <div className="px-[15px] md:px-2.5 z-10 max-w-[1920px] mx-auto bg-light-bg-primary dark:bg-dark-bg-primary">
-        <div className="flex justify-between py-0 lg:py-2.5 md:py-[7.5px] ">
+        <div className="flex justify-between py-0 lg:py-2.5 md:py-[7.5px] min-h-[55px]">
           <div className="flex items-center cursor-pointer min-w-fit md:min-w-[25px] h-[65px] lg:h-auto">
             <NextChakraLink
               href="/"
