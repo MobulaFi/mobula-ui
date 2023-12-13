f99524361a80e041bf8102ba6684ea5a1be09bfa
Veeno <sebastien.cloiseau@gmail.com>
2023-12-13 11:20:38 +0100

    fix: chart color on token fees protocol + team member table

--- src/features/dao/protocol/constants.tsx
+++ src/features/dao/protocol/constants.tsx
@@ -1,8 +1,8 @@
 export const colors = [
-  "bg-[#F7BA1E] dark:bg-[#F7BA1E]",
-  "bg-[#D91AD9] dark:bg-[#D91AD9]",
-  "bg-[#165DFF] dark:bg-[#165DFF]",
-  "bg-[#722ED1] dark:bg-[#722ED1]",
-  "bg-red dark:bg-red",
-  "bg-green dark:bg-green",
+  "#F7BA1E",
+  "#D91AD9",
+  "#165DFF",
+  "#722ED1",
+  "#ea3943",
+  "#0ECB81",
 ];
