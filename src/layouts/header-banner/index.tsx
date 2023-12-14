6cd5c21798321a902902404e8245d0a553b97642
Veeno <sebastien.cloiseau@gmail.com>
2023-12-14 19:17:30 +0100

    feature: banner header & animation

---
+++ src/layouts/header-banner/index.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { useEffect } from "react";
+import { Asset } from "../../interfaces/assets";
+import { getTokenPercentage } from "../../utils/formaters";
+
+export const HeaderBanner = ({ assets }: { assets: Asset[] }) => {
+  const animationRef = React.useRef(null);
+
+  useEffect(() => {
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      const scroller = animationRef.current;
+      scroller.setAttribute("data-animated", "true");
+      const scrollerInner: any = scroller.querySelector(
+        ".scrollerAnimated-inner"
+      );
+      const scrollerContent: any = Array.from(scrollerInner.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicated: any = item?.cloneNode(true);
+        duplicated.setAttribute("aria-hidden", "true");
+        scrollerInner.appendChild(duplicated);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="h-[40px] w-full px-[15px] flex justify-between border-b border-light-border-primary dark:border-dark-border-primary">
+      <div
+        className="scrollerAnimated flex w-full"
+        ref={animationRef}
+        data-speed="slow"
+      >
+        <div className="scrollerAnimated-inner flex">
+          {assets
+            ?.filter((entry) => entry.price_change_24h < 1000)
+            .map((asset) => {
+              const isUp = getTokenPercentage(asset?.price_change_24h) > 0;
+              return (
+                <div
+                  key={asset?.id}
+                  className={`flex items-center justify-center h-full w-full mx-1`}
+                >
+                  <span className="w-[18px] h-[18px] min-w-[18px] min-h-[18px] rounded-full bg-[#fff] dark:bg-[#fff]">
+                    <img
+                      src={asset?.logo}
+                      alt="blockchain"
+                      className="rounded-full w-full h-full shadow-2xl"
+                    />{" "}
+                  </span>
+                  <p
+                    className={`${
+                      isUp
+                        ? "text-green dark:text-green"
+                        : "text-red dark:text-red"
+                    } text-[13px] ml-[5px]`}
+                  >
+                    {isUp
+                      ? `+${getTokenPercentage(asset?.price_change_24h)}`
+                      : getTokenPercentage(asset?.price_change_24h)}
+                    %
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="bg-light-bg-primary dark:bg-red pl-5 h-full w-fit">
+        firofrsroorsfr
+      </div>
+    </div>
+  );
+};
--- src/layouts/layout/index.tsx
+++ src/layouts/layout/index.tsx
@@ -2,17 +2,34 @@ import { cookies } from "next/headers";
 import NextTopLoader from "nextjs-toploader";
 import React from "react";
 import { DexDrawer } from "../../drawer/dex";
+import { createSupabaseDOClient } from "../../lib/supabase";
 import { Footer } from "../footer";
 import { Header } from "../header";
+import { HeaderBanner } from "../header-banner";
 import { MenuFixedMobile } from "../menu-mobile";
+
+export const dynamic = "force-static";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const fetchAssets = async () => {
+  const supabasse = createSupabaseDOClient();
+  const { data, error } = await supabasse
+    .from("assets")
+    .select("logo,price_change_24h,price")
+    .order("global_volume", { ascending: false })
+    .limit(50);
+  if (error) return [];
+  return data;
+};
+
+const Layout = async ({ children }: LayoutProps) => {
   const cookieStore = cookies();
   const addressCookie = cookieStore["address"]?.value;
+  const assets = await fetchAssets();
+  console.log("datadatadata", assets);
 
   return (
     <div className="bg-light-bg-primary dark:bg-dark-bg-primary">
@@ -23,6 +40,7 @@ const Layout = ({ children }: LayoutProps) => {
         crawl={true}
       />
       <DexDrawer />
+      <HeaderBanner assets={assets || []} />
       <Header addressCookie={addressCookie || ""} />
       <div className="w-full min-h-[70vh]">{children}</div>
       <MenuFixedMobile />
