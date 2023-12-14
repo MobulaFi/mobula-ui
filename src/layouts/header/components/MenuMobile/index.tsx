1f8d30d00fcdfdbcf5748e03345541b499fb320f
Veeno <sebastien.cloiseau@gmail.com>
2023-12-14 23:54:59 +0100

    style: menu mobile position top & remove second header buttons on mobile

--- src/layouts/header/components/MenuMobile/index.tsx
+++ src/layouts/header/components/MenuMobile/index.tsx
@@ -66,7 +66,7 @@ export const MenuMobile = ({
 
   return (
     <div
-      className={`bg-light-bg-primary dark:bg-dark-bg-primary z-[110] pt-[15px] top-[45px] 
+      className={`bg-light-bg-primary dark:bg-dark-bg-primary z-[110] pt-[15px] top-[98px] 
     w-screen left-0 h-screen text-light-font-100 dark:text-dark-font-100 flex flex-col 
     overflow-x-hidden fixed ${isMenuMobile ? "flex" : "hidden"}`}
       id="mobileNav"
