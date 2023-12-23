e1c61caee6bdc754ff5058dd94e08224d866eab3
Veeno <sebastien.cloiseau@gmail.com>
2023-12-23 03:10:27 +0100

    feature: indexing supercharged mobile

--- src/features/landings/home/components/supercharged/index.tsx
+++ src/features/landings/home/components/supercharged/index.tsx
@@ -33,7 +33,7 @@ export const IndexingSupercharged = () => {
               style={{
                 WebkitTextFillColor: "transparent",
               }}
-              className="text-[72px] font-bold font-poppins w-fit mx-auto text-transparent 
+              className="text-[72px] md:text-[56px] md:leading-[56px] font-bold font-poppins w-fit mx-auto text-transparent 
                 text-fill-color tracking-[-0.08em] bg-gradient-to-br from-[rgba(0,0,0,0.95)]
                 to-[rgba(0,0,0,0.35)] dark:from-[rgba(255,255,255,0.95)]
                  dark:to-[rgba(255,255,255,0.35)] dark:text-transparent bg-clip-text"
@@ -41,20 +41,20 @@ export const IndexingSupercharged = () => {
               Indexing Supercharged
             </h1>
           </div>
-          <p className="text-light-font-60 dark:text-dark-font-60 font-[Poppins] mt-6 text-xl text-center">
+          <p className="text-light-font-60 dark:text-dark-font-60 font-poppins mt-6 text-xl md:text-base text-center md:text-start">
             A new way of using subgraphs, livestreamed, multi-chain & enriched
           </p>
           <div
-            className="p-8 flex items-center shadow-xl bg-[rgba(23, 27, 43, 0.22)] rounded-2xl backdrop-blur-md border mt-[50px] 
+            className="p-8 md:p-4 flex items-center shadow-xl bg-[rgba(23, 27, 43, 0.22)] rounded-2xl backdrop-blur-md border mt-[50px] 
                    border-light-border-primary dark:border-dark-border-primary mouse-cursor-gradient-tracking w-full h-[400px] 
-                   overflow-hidden rotating-effect"
+                   overflow-hidden rotating-effect md:h-[450px] md:flex-col md:mt-5"
             ref={liveRef}
           >
-            <div className="w-2/4 flex flex-col">
-              <h2 className="text-light-font-100 dark:text-dark-font-100 tracking-tight font-poppins text-4xl font-medium">
+            <div className="w-2/4 md:w-full flex flex-col">
+              <h2 className="text-light-font-100 dark:text-dark-font-100 tracking-tight font-poppins text-4xl md:text-2xl font-medium">
                 Livestreamed to your DB
               </h2>
-              <p className="text-light-font-60 dark:text-dark-font-60 font-[Poppins] mt-7 text-lg mb-9 max-w-[500px]">
+              <p className="text-light-font-60 dark:text-dark-font-60 font-poppins mt-7 text-lg mb-9 md:text-base md:mt-3 md:mb-4 max-w-[500px]">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
                 facilis consequuntur quasi corrupti sequi, minima sit aspernatur
                 ea ullam aut corporis dolores ut vel ratione porro voluptate
@@ -67,20 +67,20 @@ export const IndexingSupercharged = () => {
                 Read Docs
               </button>
             </div>
-            <div className="w-2/4 flex flex-col">
-              <div className="rounded-lg shadow-2xl w-fit h-fit">
-                <img
-                  className="absolute bottom-[-20px] right-[-20px]"
-                  src="/landing/supercharged/db.png"
-                />
+            <div className="w-2/4 md:w-full flex flex-col">
+              <div
+                className="rounded-lg shadow-2xl w-fit h-fit md:h-[170px] absolute bottom-[-20px]
+               right-[-20px] md:bottom-[50px] md:right-auto md:left-[30px]"
+              >
+                <img src="/landing/supercharged/db.png" />
               </div>
             </div>
           </div>
-          <div className="w-full flex justify-between">
+          <div className="w-full flex justify-between md:flex-col md:justify-start">
             <div
               className=" flex items-center shadow-xl bg-[rgba(23, 27, 43, 0.22)] rounded-2xl backdrop-blur-md border mt-[50px] 
-                   border-light-border-primary dark:border-dark-border-primary mouse-cursor-gradient-tracking w-[40%]  
-                   overflow-hidden p-5 pb-0 rotating-effect"
+                   border-light-border-primary dark:border-dark-border-primary mouse-cursor-gradient-tracking w-[40%] md:mt-5 
+                   overflow-hidden p-5 pb-0 rotating-effect md:w-full"
               ref={mulitRef}
             >
               <div className="w-full flex flex-col">
@@ -89,7 +89,7 @@ export const IndexingSupercharged = () => {
                     {firstChains?.map((content, i) => (
                       <div
                         key={content.chainId}
-                        className="flex justify-center items-center p-2.5 rounded-xl shadow-xl m-2.5 
+                        className="flex justify-center items-center p-2.5 rounded-xl shadow-xl m-2.5 md:m-1  
                    border border-light-border-primary dark:border-dark-border-primary shadow-4xl skewBox "
                         style={{
                           background:
@@ -98,7 +98,8 @@ export const IndexingSupercharged = () => {
                         }}
                       >
                         <img
-                          className="h-[45px] w-[45px] rounded-full opacity-80 shadow-4xl min-w-[45px] min-h-[45px]"
+                          className="h-[45px] w-[45px] md:w-[30px] md:h-[30px] md:min-w-[30px] md:min-h-[30px]
+                           rounded-full opacity-80 shadow-4xl min-w-[45px] min-h-[45px]"
                           src={content?.logo}
                         />
                       </div>
@@ -110,7 +111,7 @@ export const IndexingSupercharged = () => {
                     {secChains?.map((content, i) => (
                       <div
                         key={content.chainId}
-                        className="flex justify-center items-center p-2.5 rounded-xl shadow-xl m-2.5 
+                        className="flex justify-center items-center p-2.5 rounded-xl shadow-xl m-2.5 md:m-1
                  border border-light-border-primary dark:border-dark-border-primary shadow-4xl skewBox "
                         style={{
                           background:
@@ -119,7 +120,8 @@ export const IndexingSupercharged = () => {
                         }}
                       >
                         <img
-                          className="h-[45px] w-[45px] rounded-full opacity-80 shadow-4xl shadow-2xl min-w-[45px] min-h-[45px]"
+                          className="h-[45px] w-[45px] rounded-full opacity-80 shadow-4xl 
+                          shadow-2xl min-w-[45px] min-h-[45px] md:w-[30px] md:h-[30px] md:min-w-[30px] md:min-h-[30px]"
                           src={content?.logo}
                         />
                       </div>
@@ -131,7 +133,7 @@ export const IndexingSupercharged = () => {
                     {thirdChains?.map((content, i) => (
                       <div
                         key={content.chainId}
-                        className="flex justify-center items-center p-2.5 rounded-xl shadow-xl m-2.5 
+                        className="flex justify-center items-center p-2.5 rounded-xl shadow-xl m-2.5 md:m-1
                    border border-light-border-primary dark:border-dark-border-primary shadow-4xl skewBox "
                         style={{
                           background:
@@ -140,7 +142,8 @@ export const IndexingSupercharged = () => {
                         }}
                       >
                         <img
-                          className="h-[45px] w-[45px] rounded-full opacity-80 shadow-4xl shadow-2xl min-w-[45px] min-h-[45px]"
+                          className="h-[45px] w-[45px] rounded-full opacity-80 shadow-4xl shadow-2xl min-w-[45px]
+                           min-h-[45px] md:w-[30px] md:h-[30px] md:min-w-[30px] md:min-h-[30px]"
                           src={content?.logo}
                         />
                       </div>
@@ -152,7 +155,7 @@ export const IndexingSupercharged = () => {
                     {quarthChains?.map((content, i) => (
                       <div
                         key={content.chainId}
-                        className="flex justify-center items-center p-2.5 rounded-xl shadow-xl m-2.5 
+                        className="flex justify-center items-center p-2.5 rounded-xl shadow-xl m-2.5 md:m-1 
                    border border-light-border-primary dark:border-dark-border-primary shadow-4xl skewBox "
                         style={{
                           background:
@@ -161,18 +164,19 @@ export const IndexingSupercharged = () => {
                         }}
                       >
                         <img
-                          className="h-[45px] w-[45px] rounded-full opacity-80 shadow-2xl min-w-[45px] min-h-[45px]"
+                          className="h-[45px] w-[45px] rounded-full opacity-80 shadow-2xl min-w-[45px]
+                           min-h-[45px] md:w-[30px] md:h-[30px] md:min-w-[30px] md:min-h-[30px]"
                           src={content?.logo}
                         />
                       </div>
                     ))}
                   </div>
                 </div>
-                <div className="mt-8 p-3 pb-0">
-                  <h2 className="text-light-font-100 dark:text-dark-font-100 tracking-tight font-poppins text-4xl font-medium ">
+                <div className="mt-8 md:mt-4 p-3 md:p-0 pb-0">
+                  <h2 className="text-light-font-100 dark:text-dark-font-100 tracking-tight font-poppins text-4xl md:text-2xl font-medium ">
                     Multi-chain Indexing
                   </h2>
-                  <p className="text-light-font-60 dark:text-dark-font-60 font-[Poppins] mt-7 text-lg mb-9 max-w-[500px]">
+                  <p className="text-light-font-60 dark:text-dark-font-60 font-poppins mt-7 md:mt-4 text-lg mb-9 md:mb-5 md:text-base max-w-[500px]">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Dolore, facilis consequuntur quasi corrupti sequi, minima
                     sit aspernatur ea ullam aut corporis dolores ut vel ratione
@@ -184,26 +188,26 @@ export const IndexingSupercharged = () => {
             <div
               className=" flex items-center shadow-xl bg-[rgba(23, 27, 43, 0.22)] rounded-2xl backdrop-blur-md border mt-[50px] 
                    border-light-border-primary dark:border-dark-border-primary mouse-cursor-gradient-tracking w-[55%]  
-                   overflow-hidden p-5 pb-0 rotating-effect"
+                   overflow-hidden p-5 pb-0 rotating-effect md:mt-5 md:w-full md:h-[420px]"
               ref={surcharge}
             >
-              <div className="w-full flex flex-col mt-3 mb-auto">
-                <div className="p-3 pt-0">
-                  <h2 className="text-light-font-100 dark:text-dark-font-100 tracking-tight font-poppins text-4xl font-medium mt-4">
+              <div className="w-full flex flex-col mt-3 md:mt-0 mb-auto">
+                <div className="p-3 md:p-0 pt-0">
+                  <h2 className="text-light-font-100 dark:text-dark-font-100 tracking-tight font-poppins text-4xl md:text-2xl font-medium mt-4 md:mt-0">
                     Multi-chain Indexing
                   </h2>
-                  <p className="text-light-font-60 dark:text-dark-font-60 font-[Poppins] mt-7 text-lg mb-9 max-w-[500px]">
+                  <p className="text-light-font-60 dark:text-dark-font-60 font-poppins mt-7 md:mt-4 text-lg mb-9 md:text-base max-w-[500px]">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Dolore, facilis consequuntur quasi corrupti sequi, minima
                     sit aspernatur ea ullam aut corporis dolores ut vel ratione
                     porro voluptate
                   </p>{" "}
                 </div>
-                <div className="rounded-lg shadow-2xl w-fit h-fit">
-                  <img
-                    className="absolute bottom-[-10px] left-[-10px]"
-                    src="/landing/supercharged/dashboard.png"
-                  />
+                <div
+                  className="rounded-lg shadow-2xl w-fit h-fit md:h-[170px] absolute bottom-[-10px]
+               left-[-10px] md:bottom-[50px] md:right-auto md:left-[30px]"
+                >
+                  <img src="/landing/supercharged/dashboard.png" />
                 </div>
               </div>
             </div>
