c4e6f262e93e9216b0347f41b0c84847bcead3be
Veeno <sebastien.cloiseau@gmail.com>
2024-01-09 18:06:15 +0100

    feature: animation for modal

--- src/components/modal-container.tsx
+++ src/components/modal-container.tsx
@@ -18,33 +18,39 @@ export const ModalContainer = ({
   title,
   children,
 }: ModalContainerProps) => {
-  if (isOpen)
-    return (
-      <>
-        <div
-          className="z-[100] inset-0 top-0 left-0 fixed w-screen h-screen bg-background/80 
-          flex justify-center backdrop-blur-sm"
-          onClick={() => onClose()}
-        />
-        <div
-          className={cn(
-            `z-[101] fixed top-[10vh] left-1/2 -translate-x-1/2 h-fit border border-light-border-primary dark:border-dark-border-primary
+  return (
+    <>
+      <div
+        className={`z-[100] left-0 inset-0 top-0 fixed w-screen h-screen bg-background/80 
+          flex justify-center backdrop-blur-sm ${
+            isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+          } transition-all duration-200 ease-in-out`}
+        onClick={() => onClose()}
+      />
+      <div
+        className={cn(
+          `z-[101] fixed left-1/2 -translate-x-1/2 top-[10vh] h-fit border border-light-border-primary dark:border-dark-border-primary
            rounded-2xl bg-light-bg-secondary dark:bg-dark-bg-secondary text-light-font-100 dark:text-dark-font-100
-            w-full md:w-[90%] ${title ? "py-3.5 px-4" : ""} overflow-y-scroll`,
-            extraCss
-          )}
-        >
-          {title ? (
-            <div className="flex justify-between items-center mb-2.5">
-              <LargeFont>{title}</LargeFont>
-              <button onClick={() => onClose()}>
-                <AiOutlineClose className="text-light-font-100 dark:text-dark-font-100 text-xl" />
-              </button>
-            </div>
-          ) : null}
-          {children}
-        </div>
-      </>
-    );
-  return null;
+            w-full md:w-[90%] ${
+              title ? "py-3.5 px-4" : ""
+            } overflow-y-scroll  ${
+            isOpen
+              ? "scale-100 opacity-100"
+              : "opacity-0 scale-90 pointer-events-none"
+          } transition-all duration-200 ease-in-out`,
+          extraCss
+        )}
+      >
+        {title ? (
+          <div className="flex justify-between items-center mb-2.5">
+            <LargeFont>{title}</LargeFont>
+            <button onClick={() => onClose()}>
+              <AiOutlineClose className="text-light-font-100 dark:text-dark-font-100 text-xl" />
+            </button>
+          </div>
+        ) : null}
+        {children}
+      </div>
+    </>
+  );
 };
