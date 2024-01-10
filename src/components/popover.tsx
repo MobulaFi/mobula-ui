6367cd40a9b2b4fb6093e1e41f503e428b51c7a8
Veeno <sebastien.cloiseau@gmail.com>
2024-01-10 01:00:09 +0100

    feature: Improvement router selection

--- src/components/popover.tsx
+++ src/components/popover.tsx
@@ -20,21 +20,24 @@ export const Popover = ({
 }: PopoverProps) => {
   return (
     <div
-      className={`flex relative ${isFilters ? "pb-[8px]" : ""}`}
+      className={`flex relative ${isFilters ? "pb-[8px]" : ""} `}
       onMouseEnter={() => onToggle()}
       onMouseLeave={() => onToggle()}
     >
       <span className="z-[10] w-full">{visibleContent}</span>
-      {isOpen ? (
-        <div
-          className={cn(
-            "absolute border border-light-border-primary dark:border-dark-border-primary rounded-lg bg-light-bg-secondary dark:bg-dark-bg-secondary font-medium p-2.5 w-fit shadow-md z-[101]",
-            extraCss
-          )}
-        >
-          {hiddenContent}
-        </div>
-      ) : null}
+
+      <div
+        className={cn(
+          `absolute border border-light-border-primary dark:border-dark-border-primary rounded-lg bg-light-bg-secondary dark:bg-dark-bg-secondary font-medium p-2.5 w-fit shadow-md z-[101] ${
+            isOpen
+              ? "opacity-100 scale-100"
+              : "opacity-0 scale-90 pointer-events-none"
+          }`,
+          extraCss
+        )}
+      >
+        {hiddenContent}
+      </div>
     </div>
   );
 };
