79a0e8beb5645150becbbd104350be506d5e14bd
Veeno <sebastien.cloiseau@gmail.com>
2024-01-16 19:23:01 +0100

    feature: improve flow on swap asset page

--- src/layouts/swap/common/popup/settings/components/small-swap-line/index.tsx
+++ src/layouts/swap/common/popup/settings/components/small-swap-line/index.tsx
@@ -1,5 +1,11 @@
-import React, { Dispatch, RefObject, SetStateAction, useContext } from "react";
-import { BsChevronDown } from "react-icons/bs";
+import {
+  Dispatch,
+  RefObject,
+  SetStateAction,
+  useContext,
+  useEffect,
+} from "react";
+import { BiChevronDown } from "react-icons/bi";
 import { SwapContext } from "../../../../..";
 import { SmallFont } from "../../../../../../../components/fonts";
 import { getRightPrecision } from "../../../../../../../utils/formaters";
@@ -21,58 +27,66 @@ export const SmallSwapLine = ({
   const amount = position === "in" ? amountIn : amountOut;
   const setAmount = position === "in" ? setAmountIn : undefined;
 
+  useEffect(() => {
+    if (tokenIn?.balance) {
+      setAmountIn(tokenIn.balance);
+    }
+  }, [tokenIn?.balance]);
+
   return (
-    <div
-      className={`flex justify-between bg-light-bg-secondary dark:bg-dark-bg-secondary rounded-lg border
-     border-light-border-primary dark:border-dark-border-primary p-[5px] ${
+    <>
+      <div
+        className={`flex justify-between bg-light-bg-secondary dark:bg-dark-bg-secondary rounded-lg border
+     border-light-border-primary dark:border-dark-border-primary p-[5px] h-[42px] ${
        position === "out" ? "mt-[10px]" : "mt-0"
      }`}
-    >
-      <div className="flex w-full">
-        <div className="flex bg-light-bg-terciary dark:bg-dark-bg-terciary h-[30px] justify-center items-center px-[7.5px] rounded-lg">
-          <img
-            className="w-[21px] h-[21px] min-w-[21px] rounded-full"
-            src={token?.logo || "/empty/unknown.png"}
-            alt={`${token?.name} logo`}
-          />
-          <SmallFont extraCss="ml-[7.5px]">{token?.symbol}</SmallFont>
+      >
+        <div className="flex w-full">
+          <div className="flex h-[30px] justify-center items-center px-[7.5px] rounded-lg">
+            <img
+              className="w-[21px] h-[21px] min-w-[21px] rounded-full"
+              src={token?.logo || "/empty/unknown.png"}
+              alt={`${token?.name} logo`}
+            />
+            <SmallFont extraCss="ml-[7.5px]">{token?.symbol}</SmallFont>
+          </div>
+          {position === "out" ? null : (
+            <button
+              onClick={() => {
+                if (!setSelectVisible) return;
+                setSelectVisible(true);
+              }}
+            >
+              <BiChevronDown className=" text-light-font-100 dark:text-dark-font-100 text-lg" />
+            </button>
+          )}
         </div>
-        {position === "out" ? null : (
-          <button
-            className="ml-[5px]"
-            onClick={() => {
-              if (!setSelectVisible) return;
-              setSelectVisible(true);
+
+        <div className="flex">
+          <input
+            className="text-sm text-light-font-100 dark:text-dark-font-100 my-auto text-end pr-[5px] bg-light-bg-secondary dark:bg-dark-bg-secondary h-full"
+            type="number"
+            lang="en"
+            ref={inputRef}
+            onChange={(e) => {
+              if (!setAmount) return;
+              if (
+                !Number.isNaN(parseFloat(e.target.value)) ||
+                e.target.value === ""
+              ) {
+                setAmount(e.target.value);
+              }
             }}
-          >
-            <BsChevronDown className="text-sm text-light-font-100 dark:text-dark-font-100" />
-          </button>
-        )}
-      </div>
-      <div className="flex">
-        <input
-          className="text-sm text-light-font-100 dark:text-dark-font-100 my-auto text-end pr-[5px] bg-light-bg-secondary dark:bg-dark-bg-secondary"
-          type="number"
-          lang="en"
-          ref={inputRef}
-          onChange={(e) => {
-            if (!setAmount) return;
-            if (
-              !Number.isNaN(parseFloat(e.target.value)) ||
-              e.target.value === ""
-            ) {
-              setAmount(e.target.value);
+            value={
+              (typeof window !== "undefined" &&
+                inputRef?.current === document?.activeElement) ||
+              amount === ""
+                ? amount
+                : (getRightPrecision(amount) as number)
             }
-          }}
-          value={
-            (typeof window !== "undefined" &&
-              inputRef?.current === document?.activeElement) ||
-            amount === ""
-              ? amount
-              : (getRightPrecision(amount) as number)
-          }
-        />
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
