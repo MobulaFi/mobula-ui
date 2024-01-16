f93b897498f333f1e092e4a74ad7425e8f40faa8
Veeno <sebastien.cloiseau@gmail.com>
2024-01-16 05:47:59 +0100

    feature: adding error rooter handler

---
+++ src/app/global-error.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
