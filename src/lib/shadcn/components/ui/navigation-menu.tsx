f4d8b965e284d53d3b5c0853d4093696410f2699
Veeno <sebastien.cloiseau@gmail.com>
2024-01-18 07:07:55 +0100

    fix: z index for navmenu header

--- src/lib/shadcn/components/ui/navigation-menu.tsx
+++ src/lib/shadcn/components/ui/navigation-menu.tsx
@@ -11,7 +11,7 @@ const NavigationMenu = React.forwardRef<
   <NavigationMenuPrimitive.Root
     ref={ref}
     className={cn(
-      "relative z-[20] flex max-w-max flex-1 items-center justify-center",
+      "relative z-[101] flex max-w-max flex-1 items-center justify-center",
       className
     )}
     {...props}
@@ -29,7 +29,7 @@ const NavigationMenuList = React.forwardRef<
   <NavigationMenuPrimitive.List
     ref={ref}
     className={cn(
-      "group flex flex-1 list-none items-center justify-center space-x-1",
+      "group flex z-[101] flex-1 list-none items-center justify-center space-x-1",
       className
     )}
     {...props}
@@ -68,7 +68,7 @@ const NavigationMenuContent = React.forwardRef<
   <NavigationMenuPrimitive.Content
     ref={ref}
     className={cn(
-      "left-0 top-0 w-[600px] data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 absolute ",
+      "left-0 top-0 w-[600px] z-[101] data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 absolute ",
       className
     )}
     {...props}
