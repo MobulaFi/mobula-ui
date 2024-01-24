f1a3618b43a27bd5f78ab6b27da1781e9c778f24
Veeno <sebastien.cloiseau@gmail.com>
2024-01-24 15:34:34 +0100

    feature: context general

---
+++ src/contexts/general.tsx
@@ -0,0 +1,24 @@
+import React, { useMemo, useState } from "react";
+
+interface GeneralContextProps {
+  editAssetReducer: any;
+  setEditAssetReducer: React.Dispatch<any>;
+}
+
+export const GeneralContext = React.createContext({} as GeneralContextProps);
+
+export const GeneralProvider = ({ children }) => {
+  const [editAssetReducer, setEditAssetReducer] = useState<any>(null);
+
+  const value = useMemo(
+    () => ({
+      editAssetReducer,
+      setEditAssetReducer,
+    }),
+    [editAssetReducer, setEditAssetReducer]
+  );
+
+  return (
+    <GeneralContext.Provider value={value}>{children}</GeneralContext.Provider>
+  );
+};
