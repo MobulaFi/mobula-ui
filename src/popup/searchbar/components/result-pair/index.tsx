763a0d1b23f777a4e412ad78bf8b0b959acf6441
Veeno <sebastien.cloiseau@gmail.com>
2024-01-31 18:06:47 +0100

    feature: hide BNB pairs

--- src/popup/searchbar/components/result-pair/index.tsx
+++ src/popup/searchbar/components/result-pair/index.tsx
@@ -29,70 +29,76 @@ export const PairResult = ({ setTrigger, firstIndex }) => {
       {pairs?.length > 0 && (
         <Title extraCss="mt-[5px]">Pairs ({pairs?.length})</Title>
       )}
-      {pairs?.map((result, index) => {
-        const pair = result?.pairs?.[0];
-        return (
-          <div
-            className={`flex items-center cursor-pointer transition-all duration-200 justify-between ${
-              active === index + firstIndex
-                ? "bg-light-bg-hover dark:bg-dark-bg-hover"
-                : ""
-            } py-[7px] px-[20px] md:px-2.5 hover:bg-light-bg-hover hover:dark:bg-dark-bg-hover `}
-            onMouseOver={() => setActive(index + firstIndex)}
-            onClick={() => clickEvent(pair)}
-          >
-            <div className="flex items-center">
-              {(isBaseToken(result, pair) && pair?.token0?.logo) ||
-              (!isBaseToken(result, pair) && pair?.token1?.logo) ? (
-                <img
-                  src={
-                    isBaseToken(result, pair)
-                      ? pair?.token0?.logo
-                      : pair?.token1?.logo
-                  }
-                  className="w-[20px] h-[20px] rounded-full mr-[7.5px]"
-                />
-              ) : (
-                <AddressAvatar
-                  address={pair?.address as string}
-                  extraCss="w-[20px] h-[20px] rounded-full mr-[7.5px]"
-                />
-              )}
-              {(isQuoteToken(result, pair) && pair?.token0?.logo) ||
-              (!isQuoteToken(result, pair) && pair?.token1?.logo) ? (
-                <img
-                  src={
-                    isQuoteToken(result, pair)
-                      ? pair?.token0?.logo || "/empty/unknown.png"
-                      : pair?.token1?.logo || "/empty/unknown.png"
-                  }
-                  className="w-[20px] h-[20px] rounded-full mr-[7.5px]"
-                />
-              ) : (
-                <AddressAvatar
-                  address={pair?.address as string}
-                  extraCss="w-[20px] h-[20px] rounded-full mr-[7.5px]"
-                />
-              )}
-              <p className="text-sm font-medium md:font-normal max-w-[340px] truncate text-light-font-100 dark:text-dark-font-100 mr-2.5">
-                {isBaseToken(result, pair)
-                  ? pair?.token0?.symbol?.toUpperCase()
-                  : pair?.token1?.symbol?.toUpperCase()}{" "}
-                /{" "}
-                {isQuoteToken(result, pair)
-                  ? pair?.token0?.symbol?.toUpperCase()
-                  : pair?.token1?.symbol?.toUpperCase()}
+      {pairs
+        ?.filter(
+          (entry) =>
+            entry?.pairs?.[0]?.token0?.symbol !== "BNB" &&
+            entry?.pairs?.[0]?.token1?.symbol !== "BNB"
+        )
+        .map((result, index) => {
+          const pair = result?.pairs?.[0];
+          return (
+            <div
+              className={`flex items-center cursor-pointer transition-all duration-200 justify-between ${
+                active === index + firstIndex
+                  ? "bg-light-bg-hover dark:bg-dark-bg-hover"
+                  : ""
+              } py-[7px] px-[20px] md:px-2.5 hover:bg-light-bg-hover hover:dark:bg-dark-bg-hover `}
+              onMouseOver={() => setActive(index + firstIndex)}
+              onClick={() => clickEvent(pair)}
+            >
+              <div className="flex items-center">
+                {(isBaseToken(result, pair) && pair?.token0?.logo) ||
+                (!isBaseToken(result, pair) && pair?.token1?.logo) ? (
+                  <img
+                    src={
+                      isBaseToken(result, pair)
+                        ? pair?.token0?.logo
+                        : pair?.token1?.logo
+                    }
+                    className="w-[20px] h-[20px] rounded-full mr-[7.5px]"
+                  />
+                ) : (
+                  <AddressAvatar
+                    address={pair?.address as string}
+                    extraCss="w-[20px] h-[20px] rounded-full mr-[7.5px]"
+                  />
+                )}
+                {(isQuoteToken(result, pair) && pair?.token0?.logo) ||
+                (!isQuoteToken(result, pair) && pair?.token1?.logo) ? (
+                  <img
+                    src={
+                      isQuoteToken(result, pair)
+                        ? pair?.token0?.logo || "/empty/unknown.png"
+                        : pair?.token1?.logo || "/empty/unknown.png"
+                    }
+                    className="w-[20px] h-[20px] rounded-full mr-[7.5px]"
+                  />
+                ) : (
+                  <AddressAvatar
+                    address={pair?.address as string}
+                    extraCss="w-[20px] h-[20px] rounded-full mr-[7.5px]"
+                  />
+                )}
+                <p className="text-sm font-medium md:font-normal max-w-[340px] truncate text-light-font-100 dark:text-dark-font-100 mr-2.5">
+                  {isBaseToken(result, pair)
+                    ? pair?.token0?.symbol?.toUpperCase()
+                    : pair?.token1?.symbol?.toUpperCase()}{" "}
+                  /{" "}
+                  {isQuoteToken(result, pair)
+                    ? pair?.token0?.symbol?.toUpperCase()
+                    : pair?.token1?.symbol?.toUpperCase()}
+                </p>
+              </div>
+              <p className="text-sm font-medium md:font-normal max-w-[340px] truncate text-light-font-100 dark:text-dark-font-100">
+                <span className="text-light-font-40 dark:text-dark-font-40 mr-1">
+                  Liquidity:
+                </span>{" "}
+                ${getFormattedAmount(pair?.liquidity)}
               </p>
             </div>
-            <p className="text-sm font-medium md:font-normal max-w-[340px] truncate text-light-font-100 dark:text-dark-font-100">
-              <span className="text-light-font-40 dark:text-dark-font-40 mr-1">
-                Liquidity:
-              </span>{" "}
-              ${getFormattedAmount(pair?.liquidity)}
-            </p>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 };
