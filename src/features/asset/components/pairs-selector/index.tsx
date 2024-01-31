4ff3eb44bfd240bb9b881638daa7ca520620aa7d
Veeno <sebastien.cloiseau@gmail.com>
2024-01-31 18:15:32 +0100

    feature: handle no pairs found

--- src/features/asset/components/pairs-selector/index.tsx
+++ src/features/asset/components/pairs-selector/index.tsx
@@ -33,6 +33,11 @@ export const PairsSelector = () => {
     if (baseAsset) fetchPairs();
   }, [baseAsset]);
 
+  const showPopover =
+    assetPairs?.pairs?.filter(
+      (pair) => pair?.token1?.symbol !== "BNB" && pair?.token0?.symbol !== "BNB"
+    )?.length > 0;
+
   return (
     <Popover
       visibleContent={
@@ -83,86 +88,94 @@ export const PairsSelector = () => {
                 extraCss="ml-1"
               />
             </div>
-            <BiChevronDown className="text-xl text-light-font-60 dark:text-dark-font-60 mx-2 md:mr-0" />
+            {showPopover ? (
+              <BiChevronDown className="text-xl text-light-font-60 dark:text-dark-font-60 mx-2 md:mr-0" />
+            ) : null}
           </div>
         </div>
       }
       hiddenContent={
         <div className="flex flex-col min-w-[300px] max-h-[410px] w-full overflow-y-scroll scroll">
-          {assetPairs?.pairs?.map((pair, i) => (
-            <Link href={`/pair/${pair.address}`} key={i}>
-              <div
-                className="flex items-center justify-between p-2.5 hover:bg-light-bg-hover hover:dark:bg-dark-bg-hover cursor-pointer
+          {assetPairs?.pairs
+            ?.filter(
+              (pair) =>
+                pair?.token1?.symbol !== "BNB" && pair?.token0?.symbol !== "BNB"
+            )
+            ?.map((pair, i) => (
+              <Link href={`/pair/${pair.address}`} key={i}>
+                <div
+                  className="flex items-center justify-between p-2.5 hover:bg-light-bg-hover hover:dark:bg-dark-bg-hover cursor-pointer
                transition-all duration-200 ease-linear rounded-md"
-              >
-                <div className="flex items-center">
-                  <div className="relative w-fit h-fit mr-5">
-                    <img
-                      className="w-[34px] h-[34px] min-w-[34px] lg:w-[22px] lg:h-[22px] lg:min-w-[22px] md:w-[20px] md:h-[20px]
+                >
+                  <div className="flex items-center">
+                    <div className="relative w-fit h-fit mr-5">
+                      <img
+                        className="w-[34px] h-[34px] min-w-[34px] lg:w-[22px] lg:h-[22px] lg:min-w-[22px] md:w-[20px] md:h-[20px]
                      md:min-w-[20px] rounded-full bg-light-bg-hover dark:bg-dark-bg-hover border-2 border-blue dark:border-blue"
-                      src={
-                        pair?.[baseAsset?.quoteToken]?.logo ||
-                        "/empty/unknown.png"
-                      }
-                      alt={`${baseAsset?.name} logo`}
-                    />
-                    <img
-                      className="w-[20px] h-[20px] min-w-[20px] rounded-full absolute bottom-[-2px] right-[-5px] shadow-xl 
+                        src={
+                          pair?.[baseAsset?.quoteToken]?.logo ||
+                          "/empty/unknown.png"
+                        }
+                        alt={`${baseAsset?.name} logo`}
+                      />
+                      <img
+                        className="w-[20px] h-[20px] min-w-[20px] rounded-full absolute bottom-[-2px] right-[-5px] shadow-xl 
                   border-2 border-light-border-primary dark:border-dark-border-primary bg-light-bg-hover dark:bg-dark-bg-hover"
-                      src={
-                        famousContractsLabelFromName[pair?.protocol]?.logo ||
-                        "/empty/unknown.png"
-                      }
-                      alt={`${baseAsset?.name} logo`}
-                    />
+                        src={
+                          famousContractsLabelFromName[pair?.protocol]?.logo ||
+                          "/empty/unknown.png"
+                        }
+                        alt={`${baseAsset?.name} logo`}
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <LargeFont extraCss="leading-tight">
+                        <span className="font-medium">
+                          {" "}
+                          {pair?.[baseAsset?.baseToken]?.symbol}
+                        </span>{" "}
+                        <span className="text-light-font-60 dark:text-dark-font-60 font-normal">
+                          / {pair?.[baseAsset?.quoteToken]?.symbol}
+                        </span>
+                      </LargeFont>
+                      <div className="flex items-center">
+                        <SmallFont extraCss="text-light-font-40 dark:text-dark-font-40 text-start text-xs">
+                          Liquidity:
+                        </SmallFont>
+                        <SmallFont extraCss="text-start text-xs ml-2">
+                          ${getFormattedAmount(pair?.liquidity, 2)}
+                        </SmallFont>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex flex-col">
-                    <LargeFont extraCss="leading-tight">
-                      <span className="font-medium">
-                        {" "}
-                        {pair?.[baseAsset?.baseToken]?.symbol}
-                      </span>{" "}
-                      <span className="text-light-font-60 dark:text-dark-font-60 font-normal">
-                        / {pair?.[baseAsset?.quoteToken]?.symbol}
-                      </span>
+                  <div className="flex flex-col items-end ml-5">
+                    <LargeFont extraCss="leading-tight text-light-font-80 dark:text-dark-font-80">
+                      $
+                      {getFormattedAmount(
+                        pair?.price,
+                        2,
+                        {
+                          minifyZeros: true,
+                          minifyBigNumbers: true,
+                        },
+                        true
+                      )}
                     </LargeFont>
                     <div className="flex items-center">
                       <SmallFont extraCss="text-light-font-40 dark:text-dark-font-40 text-start text-xs">
-                        Liquidity:
+                        Volume:
                       </SmallFont>
                       <SmallFont extraCss="text-start text-xs ml-2">
-                        ${getFormattedAmount(pair?.liquidity, 2)}
+                        ${getFormattedAmount(pair?.volume, 2)}
                       </SmallFont>
                     </div>
                   </div>
-                </div>
-                <div className="flex flex-col items-end ml-5">
-                  <LargeFont extraCss="leading-tight text-light-font-80 dark:text-dark-font-80">
-                    $
-                    {getFormattedAmount(
-                      pair?.price,
-                      2,
-                      {
-                        minifyZeros: true,
-                        minifyBigNumbers: true,
-                      },
-                      true
-                    )}
-                  </LargeFont>
-                  <div className="flex items-center">
-                    <SmallFont extraCss="text-light-font-40 dark:text-dark-font-40 text-start text-xs">
-                      Volume:
-                    </SmallFont>
-                    <SmallFont extraCss="text-start text-xs ml-2">
-                      ${getFormattedAmount(pair?.volume, 2)}
-                    </SmallFont>
-                  </div>
-                </div>
-              </div>{" "}
-            </Link>
-          ))}
+                </div>{" "}
+              </Link>
+            ))}
         </div>
       }
+      showContent={showPopover}
     />
   );
 };
