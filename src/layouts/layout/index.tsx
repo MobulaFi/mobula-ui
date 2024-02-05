a3f337e50b628ed6751ba369f40c0f1a1133cbfb
Veeno <sebastien.cloiseau@gmail.com>
2024-02-05 10:02:56 +0100

    feature: nav position fixed to not move the app size

--- src/layouts/layout/index.tsx
+++ src/layouts/layout/index.tsx
@@ -41,7 +41,7 @@ const Layout = async ({ children }: LayoutProps) => {
       <HeaderBanner assets={assets || []} />
       <div className="flex">
         <Nav />
-        <div className="w-full overflow-scroll h-screen" id="app">
+        <div className="w-full overflow-scroll h-screen ml-[68px]" id="app">
           <Header addressCookie={addressCookie || ""} />
           <div className="w-full min-h-[70vh]">{children}</div> <Footer />
         </div>
