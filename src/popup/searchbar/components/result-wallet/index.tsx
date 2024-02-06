92120a3c740c699e80bac0568bca265a113046f5
Veeno <sebastien.cloiseau@gmail.com>
2024-02-06 17:48:07 +0100

    fix: double pairs

--- src/popup/searchbar/components/result-wallet/index.tsx
+++ src/popup/searchbar/components/result-wallet/index.tsx
@@ -45,7 +45,7 @@ export const WalletResult = ({
   };
 
   return (
-    <div className={`${results.length > 0 ? "mt-[10px]" : "mt-0"}`}>
+    <div className={`${users.length > 0 ? "mt-[10px]" : "mt-0 hidden"}`}>
       {users.length > 0 && (
         <Title extraCss="mt-[5px]">Wallets ({users.length})</Title>
       )}
