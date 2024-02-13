e070c53e0ab28e2cd51c3565b5725944c7dbff95
Veeno <sebastien.cloiseau@gmail.com>
2024-02-13 20:17:22 +0100

    feature: adding WSS for refresh pairs

--- src/features/data/chains/components/table-body/index.tsx
+++ src/features/data/chains/components/table-body/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/navigation";
-import React from "react";
+import { useEffect, useState } from "react";
 import { AddressAvatar } from "../../../../../components/avatar";
 import { SmallFont } from "../../../../../components/fonts";
 import { TagPercentage } from "../../../../../components/tag-percentage";
@@ -10,138 +10,184 @@ import {
   getFormattedAmount,
 } from "../../../../../utils/formaters";
 import { useChains } from "../../context-manager";
+import { PairsProps } from "../../models";
 
 export const TableTbody = () => {
-  const { pairs } = useChains();
+  const { pairs: pairsBuffer } = useChains();
   const router = useRouter();
+  const [pairs, setPairs] = useState<PairsProps[]>(pairsBuffer);
+
+  useEffect(() => {
+    const socket = new WebSocket(
+      process.env.NEXT_PUBLIC_PRICE_WSS_ENDPOINT as string
+    );
+
+    socket.addEventListener("open", () => {
+      socket.send(
+        JSON.stringify({
+          type: "pair",
+          authorization: process.env.NEXT_PUBLIC_PRICE_KEY,
+          payload: { blockchain: pairs?.[0]?.pair?.blockchain, interval: 5 },
+        })
+      );
+    });
+
+    socket.addEventListener("message", (event) => {
+      const { data } = JSON.parse(event.data);
+      console.log("I change pairs");
+      if (data?.length === 0) return;
+      setPairs((prevPairs) => {
+        return prevPairs?.map((pair) => {
+          const foundData = data?.find(
+            (d) => d?.pair?.address === pair?.pair?.address
+          );
+          if (foundData) {
+            return {
+              ...pair,
+              price: foundData.price,
+              last_trade: foundData.last_trade,
+              price_change_5min: foundData.price_change_5min,
+              price_change_1h: foundData.price_change_1h,
+              price_change_4h: foundData.price_change_4h,
+              price_change_24h: foundData.price_change_24h,
+            };
+          } else {
+            return pair;
+          }
+        });
+      });
+    });
+  }, []);
 
   return (
     <>
-      {pairs?.map((item, i) => {
-        const pair = item?.pair;
-        const timeAgo = useTimeAgo(item?.last_trade);
-        if (pair?.[pair?.baseToken]?.symbol === "SPONGE")
-          console.log("item?.price_change_24h", item?.price_change_24h);
-
-        return (
-          <tbody
-            key={i}
-            onClick={() => router.push(`/pair/${pair?.address}`)}
-            className="cursor-pointer hover:bg-light-bg-terciary hover:dark:bg-dark-bg-terciary transition-all duration-100 ease-linear"
-          >
-            <Segment>
-              <div className="flex items-center">
-                <div className="flex items-center md:flex-col md:items-start">
-                  <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-start">
-                    {pair?.[pair?.baseToken]?.symbol} /{" "}
-                    <span className="text-light-font-60 dark:text-dark-font-60">
-                      {pair?.[pair?.quoteToken]?.symbol}
-                    </span>
-                  </SmallFont>
-                  <div className="flex items-center">
-                    {pair?.[pair?.baseToken]?.logo ? (
-                      <img
-                        className="w-[20px] h-[20px] md:w-[15px] md:h-[15px] rounded-full"
-                        alt="token logo"
-                        src={pair?.[pair?.baseToken]?.logo || ""}
-                      />
-                    ) : (
-                      <AddressAvatar
-                        address={pair?.address}
-                        extraCss="w-[20px] h-[20px] md:w-[15px] md:h-[15px] rounded-full"
-                      />
-                    )}
-                    <SmallFont extraCss="w-fit ml-2 md:ml-1 whitespace-nowrap text-start max-w-[150px] truncate">
-                      {pair?.[pair?.baseToken]?.name}
+      {pairs
+        ?.filter((entry) => entry.price !== 0)
+        ?.map((item, i) => {
+          const pair = item?.pair;
+          const timeAgo = useTimeAgo(item?.last_trade);
+          return (
+            <tbody
+              key={i}
+              onClick={() => router.push(`/pair/${pair?.address}`)}
+              className="cursor-pointer hover:bg-light-bg-terciary hover:dark:bg-dark-bg-terciary transition-all duration-100 ease-linear animate-fadeInTrade"
+            >
+              <Segment>
+                <div className="flex items-center">
+                  <div className="flex items-center md:flex-col md:items-start">
+                    <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-start">
+                      {pair?.[pair?.baseToken]?.symbol} /{" "}
+                      <span className="text-light-font-60 dark:text-dark-font-60">
+                        {pair?.[pair?.quoteToken]?.symbol}
+                      </span>
                     </SmallFont>
+                    <div className="flex items-center">
+                      {pair?.[pair?.baseToken]?.logo ? (
+                        <img
+                          className="w-[20px] h-[20px] md:w-[15px] md:h-[15px] rounded-full"
+                          alt="token logo"
+                          src={pair?.[pair?.baseToken]?.logo || ""}
+                        />
+                      ) : (
+                        <AddressAvatar
+                          address={pair?.address}
+                          extraCss="w-[20px] h-[20px] md:w-[15px] md:h-[15px] rounded-full"
+                        />
+                      )}
+                      <SmallFont extraCss="w-fit ml-2 md:ml-1 whitespace-nowrap text-start max-w-[150px] truncate">
+                        {pair?.[pair?.baseToken]?.name}
+                      </SmallFont>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Segment>
-            <Segment>
-              {" "}
-              <div className="w-full flex justify-end">
-                <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
-                  $
-                  {getFormattedAmount(item?.price, 0, {
-                    canUseHTML: true,
-                  })}
-                </SmallFont>
-              </div>
-            </Segment>
-            {/* <Segment>
-                            <div className="w-full flex justify-end">
-                              <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
-                                {getFormattedAmount(pair?.trades)}
-                              </SmallFont>
-                            </div>
-                          </Segment> */}
-            <Segment>
-              <div className="w-full flex justify-end">
-                <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
-                  ${getFormattedAmount(pair?.volume24h)}
-                </SmallFont>
-              </div>
-            </Segment>
-            <Segment>
-              <div className="w-full flex justify-end">
-                <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
-                  ${getFormattedAmount(pair?.liquidity)}
-                </SmallFont>
-              </div>
-            </Segment>
-            <Segment>
-              <div className="w-full flex justify-end">
-                <TagPercentage
-                  isUp={item?.price_change_5min > 0 || false}
-                  percentage={convertScientificNotation(
-                    item?.price_change_5min
-                  )}
-                  inhert={
-                    item?.price_change_5min === 0 || !item?.price_change_5min
-                  }
-                />
-              </div>
-            </Segment>
-            <Segment>
-              <div className="w-full flex justify-end">
-                <TagPercentage
-                  isUp={item?.price_change_1h > 0 || false}
-                  percentage={convertScientificNotation(item?.price_change_1h)}
-                  inhert={item?.price_change_1h === 0 || !item?.price_change_1h}
-                />
-              </div>
-            </Segment>
-            <Segment>
-              <div className="w-full flex justify-end">
-                <TagPercentage
-                  isUp={item?.price_change_4h > 0 || false}
-                  percentage={convertScientificNotation(item?.price_change_4h)}
-                  inhert={item?.price_change_4h === 0 || !item?.price_change_4h}
-                />
-              </div>
-            </Segment>
-            <Segment>
-              <div className="w-full flex justify-end">
-                <TagPercentage
-                  isUp={item?.price_change_24h > 0 || false}
-                  percentage={convertScientificNotation(item?.price_change_24h)}
-                  inhert={
-                    item?.price_change_24h === 0 || !item?.price_change_24h
-                  }
-                />
-              </div>
-            </Segment>
-            <Segment>
-              <div className="w-full flex justify-end">
-                <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
-                  {timeAgo || "--"}
-                </SmallFont>
-              </div>
-            </Segment>
-          </tbody>
-        );
-      })}
+              </Segment>
+              <Segment>
+                {" "}
+                <div className="w-full flex justify-end">
+                  <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
+                    $
+                    {getFormattedAmount(item?.price, 0, {
+                      canUseHTML: true,
+                    })}
+                  </SmallFont>
+                </div>
+              </Segment>
+              <Segment>
+                <div className="w-full flex justify-end">
+                  <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
+                    ${getFormattedAmount(pair?.volume24h)}
+                  </SmallFont>
+                </div>
+              </Segment>
+              <Segment>
+                <div className="w-full flex justify-end">
+                  <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
+                    ${getFormattedAmount(pair?.liquidity)}
+                  </SmallFont>
+                </div>
+              </Segment>
+              <Segment>
+                <div className="w-full flex justify-end">
+                  <TagPercentage
+                    isUp={item?.price_change_5min > 0 || false}
+                    percentage={convertScientificNotation(
+                      item?.price_change_5min
+                    )}
+                    inhert={
+                      item?.price_change_5min === 0 || !item?.price_change_5min
+                    }
+                  />
+                </div>
+              </Segment>
+              <Segment>
+                <div className="w-full flex justify-end">
+                  <TagPercentage
+                    isUp={item?.price_change_1h > 0 || false}
+                    percentage={convertScientificNotation(
+                      item?.price_change_1h
+                    )}
+                    inhert={
+                      item?.price_change_1h === 0 || !item?.price_change_1h
+                    }
+                  />
+                </div>
+              </Segment>
+              <Segment>
+                <div className="w-full flex justify-end">
+                  <TagPercentage
+                    isUp={item?.price_change_4h > 0 || false}
+                    percentage={convertScientificNotation(
+                      item?.price_change_4h
+                    )}
+                    inhert={
+                      item?.price_change_4h === 0 || !item?.price_change_4h
+                    }
+                  />
+                </div>
+              </Segment>
+              <Segment>
+                <div className="w-full flex justify-end">
+                  <TagPercentage
+                    isUp={item?.price_change_24h > 0 || false}
+                    percentage={convertScientificNotation(
+                      item?.price_change_24h
+                    )}
+                    inhert={
+                      item?.price_change_24h === 0 || !item?.price_change_24h
+                    }
+                  />
+                </div>
+              </Segment>
+              <Segment>
+                <div className="w-full flex justify-end">
+                  <SmallFont extraCss="w-fit mr-2.5 whitespace-nowrap text-end">
+                    {timeAgo || "--"}
+                  </SmallFont>
+                </div>
+              </Segment>
+            </tbody>
+          );
+        })}
     </>
   );
 };
--- src/features/data/chains/index.tsx
+++ src/features/data/chains/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect } from "react";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Container } from "../../../components/container";
@@ -8,31 +7,8 @@ import { MiddleBox } from "./components/box-middle";
 import { RightBox } from "./components/box-right";
 import { TableTbody } from "./components/table-body";
 import { TableHeader } from "./components/table-header";
-import { useChains } from "./context-manager";
 
 export const Chains = () => {
-  const { pairs } = useChains();
-  useEffect(() => {
-    const socket = new WebSocket(
-      process.env.NEXT_PUBLIC_PRICE_WSS_ENDPOINT as string
-    );
-
-    socket.addEventListener("open", () => {
-      socket.send(
-        JSON.stringify({
-          type: "pair",
-          authorization: process.env.NEXT_PUBLIC_PRICE_KEY,
-          payload: { blockchain: "ethereum", interval: 5 },
-        })
-      );
-    });
-
-    socket.addEventListener("message", (event) => {
-      console.log("event", event);
-      const eventData = JSON.parse(event.data);
-      console.log("eventData", eventData);
-    });
-  }, []);
   return (
     <div className="flex pb-5 md:pb-2.5 items-center flex-col w-full overflow-x-hidden">
       <Container extraCss="lg:flex flex-row max-w-[1300px] justify-between mb-0 md:mb-0 pb-0 lg:w-full overflow-x-scroll">
@@ -64,7 +40,7 @@ export const Chains = () => {
       <Container extraCss="max-w-[1300px] mt-0 overflow-x-scroll">
         <TableHeader isLoading={false}>
           <TableTbody />
-        </TableHeader>{" "}
+        </TableHeader>
       </Container>
     </div>
   );
