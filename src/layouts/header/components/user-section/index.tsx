aa5345236aaa267bbed91d4f321e9c41c055b726
Veeno <sebastien.cloiseau@gmail.com>
2024-02-16 11:13:22 +0100

    feature: improvement header

--- src/layouts/header/components/user-section/index.tsx
+++ src/layouts/header/components/user-section/index.tsx
@@ -1,11 +1,10 @@
 "use client";
 import Cookies from "js-cookie";
 import { useParams, useRouter } from "next/navigation";
-import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { AiOutlineClose, AiOutlineStar } from "react-icons/ai";
 import { BsPower } from "react-icons/bs";
 import { FaTelegramPlane } from "react-icons/fa";
-import { FiSearch } from "react-icons/fi";
 import { PiUsersThreeLight } from "react-icons/pi";
 import { RiMenu3Line } from "react-icons/ri";
 import { TbBellRinging } from "react-icons/tb";
@@ -18,15 +17,11 @@ import { Popover } from "../../../../components/popover";
 import { Skeleton } from "../../../../components/skeleton";
 import { MOBL_ADDRESS } from "../../../../constants";
 import { CommonPageContext } from "../../../../contexts/commun-page";
-import {
-  PopupStateContext,
-  PopupUpdateContext,
-} from "../../../../contexts/popup";
+import { PopupUpdateContext } from "../../../../contexts/popup";
 import { UserContext } from "../../../../contexts/user";
 import { pushData } from "../../../../lib/mixpanel";
 import { Connect } from "../../../../popup/connect";
 import { FeedBackPopup } from "../../../../popup/feedback";
-import { SearchBarPopup } from "../../../../popup/searchbar";
 import { SwitchNetworkPopup } from "../../../../popup/switch-network";
 import { balanceOfAbi } from "../../../../utils/abi";
 import { addressSlicer, getFormattedAmount } from "../../../../utils/formaters";
@@ -44,21 +39,10 @@ interface UserSectionProps {
   addressFromCookie: string;
 }
 
-// const ConnectWallet = dynamic(
-//   () => import("../../../../common/components/popup/wallet-reconnect"),
-//   {
-//     ssr: false,
-//   }
-// );
-
 export const UserSection = ({ addressFromCookie }: UserSectionProps) => {
   const [triggerSearch, setTriggerSearch] = useState(false);
-  const wrapperRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
-  const [isConnectorOpen, setIsConnectorOpen] = useState(false);
   const { setConnect } = useContext(PopupUpdateContext);
-  const { connect, showCard, showConnectSocialPopup } =
-    useContext(PopupStateContext);
   const [showFeedbackPopup, setShowFeedbackPopup] = useState(false);
   const { address, isConnected } = useAccount();
   const { user } = useContext(UserContext);
@@ -232,27 +216,6 @@ export const UserSection = ({ addressFromCookie }: UserSectionProps) => {
   return (
     <>
       <div className="relative flex items-center w-full justify-end lg:justify-end">
-        <div
-          className="flex text-light-font-60 dark:text-dark-font-60 items-center rounded-md border
-            border-light-border-primary dark:border-dark-border-primary bg-light-bg-secondary 
-            dark:bg-dark-bg-secondary h-[35px] mr-2.5 md:mr-[7.5px] transition-all duration-200 
-            max-w-[350px] lg:max-w-full w-full ml-0 lg:ml-2.5 cursor-pointer 
-            hover:bg-light-bg-hover hover:dark:bg-dark-bg-hover ease-in-out overflow-hidden"
-          onClick={() => {
-            setTriggerSearch(true);
-            setIsMenuMobile(false);
-          }}
-        >
-          <div
-            className="flex border-r border-light-border-primary dark:border-dark-border-primary 
-              lg:border-transparent lg:dark:border-transparent items-center px-[7.5px] h-full rounded-l"
-          >
-            <FiSearch className="text-sm md:text-lg text-light-font-100 dark:text-dark-font-100" />
-          </div>
-          <p className="text-sm text-light-font-100 dark:text-dark-font-100 truncate pl-2 lg:pl-2.5">
-            Crypto name, wallet, ens, token address...
-          </p>
-        </div>
         {!isMenuMobile ? (
           <>
             <ChainsChanger
@@ -437,9 +400,6 @@ export const UserSection = ({ addressFromCookie }: UserSectionProps) => {
         setShowInfoPopover={setShowInfoPopover}
         showInfoPopover={showInfoPopover}
       />
-      {triggerSearch ? (
-        <SearchBarPopup trigger={triggerSearch} setTrigger={setTriggerSearch} />
-      ) : null}
       {/* {showTelegramConnector ? (
         <PopupTelegram
           showPopup={showTelegramConnector}
