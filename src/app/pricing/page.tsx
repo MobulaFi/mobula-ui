863c04f97650190ab414afae6f6e344367a9a498
Veeno <sebastien.cloiseau@gmail.com>
2024-03-06 10:02:17 +0100

    chore: pricing log

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,5 +1,15 @@
+import { Metadata } from "next";
 import React from "react";
 import { Pricing } from "../../features/landings/pricing";
+
+export const metadata: Metadata = {
+  title: "Mobula API Pricing Plans | Mobula",
+  description:
+    "Explore our flexible pricing plans for the Mobula API. Choose the one that best suits your development and growth needs. Access advanced features and dedicated support to propel your projects to new heights.",
+  robots: "index, follow",
+  keywords:
+    "Mobula, Mobula crypto, Crypto API, API, Mobula API, Mobula API Pricing, Mobula API Pricing Plan",
+};
 
 export default function PricingPage() {
   return (
