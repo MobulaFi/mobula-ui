f4b34c373004fd69059af21de6b54109b43340c9
xyeuu <teoconstant3@gmail.com>
2024-03-11 18:48:19 +0100

    fix: new pricing listing form

--- src/features/misc/listing-form/components/submit/index.tsx
+++ src/features/misc/listing-form/components/submit/index.tsx
@@ -234,7 +234,7 @@ export const Submit = ({ state }) => {
                   </div>
                 </SmallFont>
                 <SmallFont extraCss="text-center mt-4">
-                  Price: $50 (stablecoin only)
+                  Price: $150 (stablecoin only)
                 </SmallFont>
               </div>
               <div className="border border-light-border-primary dark:border-dark-border-primary rounded-md p-4 w-[500px]">
@@ -251,12 +251,12 @@ export const Submit = ({ state }) => {
                   </div>
                 </SmallFont>
                 <SmallFont extraCss="text-center mt-4">
-                  Price: $150 (stablecoin only)
+                  Price: $500 (stablecoin only)
                 </SmallFont>
               </div>
             </div>
             <SmallFont extraCss="mt-6">
-              Send $50 for Standard Listing or $150 for Express Listing to the
+              Send $150 for Standard Listing or $500 for Express Listing to the
               following Ethereum address:
               <div className="flex mt-1 md:ml-0">
                 (Supported chains: Polygon, BSC, Ethereum)
